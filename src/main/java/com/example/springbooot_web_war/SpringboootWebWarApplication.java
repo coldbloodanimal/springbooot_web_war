49a47add907cc6ba1a8fa39c0f46d02604e66299
monster <dang7758@126.com>
2022-06-22 15:38:44 +0800

    Initial commit

---
+++ src/main/java/com/example/springbooot_web_war/SpringboootWebWarApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbooot_web_war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboootWebWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboootWebWarApplication.class, args);
+    }
+
+}
